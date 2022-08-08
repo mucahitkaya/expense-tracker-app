f95996c034d356de85377f1a7290577849920a66
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-08-08 22:38:04 +0300

    styling-error in ExpenseItem.js

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -8,31 +8,31 @@ const DUMMY_EXPENSES = [
     id: "e1",
     description: "A pair of shoes",
     amount: 56.88,
-    date: new Date("2021-12-4"),
+    date: new Date(2021, 12, 4),
   },
   {
     id: "e2",
     description: "A pair of tshirts",
     amount: 23.8,
-    date: new Date("2022-11-5"),
+    date: new Date(2022, 11, 5),
   },
   {
     id: "e3",
     description: "A pair of book",
     amount: 12.88,
-    date: new Date("2012-3-5"),
+    date: new Date(2012, 3, 5),
   },
   {
     id: "e4",
     description: "A pair of pencils",
     amount: 67.88,
-    date: new Date("2005-2-9"),
+    date: new Date(2005, 2, 9),
   },
   {
     id: "e5",
     description: "A pair of bottles",
     amount: 99.8,
-    date: new Date("2012-6-6"),
+    date: new Date(2012, 6, 6),
   },
 ];
 
